ed93c63cb58630f4691e4bb29cef45ba2fd6cc3f
Matheus Silva Corradi <matheus.corradi@aluno.ufop.edu.br>
2022-08-10 13:09:15 -0300

    primeiro commit

---
+++ src/GraphAlgorithms/Graph.java
@@ -0,0 +1,61 @@
+package GraphAlgorithms;
+
+public class Graph {
+    //contadores
+    private int countNodes;
+    private int countEdges;
+    private int[][] adjMatrix;
+
+    public Graph(int countNodes) {
+        this.countNodes = countNodes;
+        this.adjMatrix = new int[countNodes][countNodes];
+    }
+
+    public int getCountNodes() {
+        return this.countNodes;
+    }
+
+    public int getCountEdges() {
+        return this.countEdges;
+    }
+
+    public int[][] getAdjMatrix() {
+        return this.adjMatrix;
+    }
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < this.adjMatrix.length; ++i) {
+            for (int j = 0; j < this.adjMatrix[i].length; ++j) {
+                str += this.adjMatrix[i][j] + "\t";
+            }
+            str += "\n";
+        }
+        return str;
+    }
+
+
+            public void addEdge(int source, int sink, int weight) {
+                if (source < 0 || source > this.countNodes - 1
+                        || sink < 0 || sink > this.countNodes - 1 || weight <= 0) {
+                    System.err.println("Invalid value for source, sink or weight!");
+                    return;
+                }
+                this.adjMatrix[source][sink] = weight;
+                this.countEdges++;
+            }
+
+    public int degree (int node){
+    int degree = 0;
+    for (int j = 0; j < this.adjMatrix[node].length; ++j) {
+        if (this.adjMatrix[node][j] !=0)
+            ++degree;
+        }
+
+        return degree;
+    }
+
+//criar um projeto repoositorio no github
+// postar no moodle com o link
+
+
+}
