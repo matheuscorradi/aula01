82d3b1fc7c79d296027539ee6806e7648191d8b5
Matheus Silva Corradi <matheus.corradi@aluno.ufop.edu.br>
2022-08-12 16:15:43 -0300

    Mudança no cod raiz, com adicionanmento de maior e menor grau mais grafo complementar

--- src/GraphAlgorithms/Graph.java
+++ src/GraphAlgorithms/Graph.java
@@ -1,6 +1,7 @@
 package GraphAlgorithms;
 
 public class Graph {
+
     //contadores
     private int countNodes;
     private int countEdges;
@@ -14,11 +15,9 @@ public class Graph {
     public int getCountNodes() {
         return this.countNodes;
     }
-
     public int getCountEdges() {
         return this.countEdges;
     }
-
     public int[][] getAdjMatrix() {
         return this.adjMatrix;
     }
@@ -33,17 +32,17 @@ public class Graph {
         return str;
     }
 
+    //funcao para adicioanr arestas
+    public void addEdge(int source, int sink, int weight) {
+        if (source < 0 || source > this.countNodes - 1 || sink < 0 || sink > this.countNodes - 1 || weight <= 0) {
+            System.err.println("Valor inválido para fonte, coletor ou peso!");
+            return;
+        }
+        this.adjMatrix[source][sink] = weight;
+        this.countEdges++;
+    }
 
-            public void addEdge(int source, int sink, int weight) {
-                if (source < 0 || source > this.countNodes - 1
-                        || sink < 0 || sink > this.countNodes - 1 || weight <= 0) {
-                    System.err.println("Invalid value for source, sink or weight!");
-                    return;
-                }
-                this.adjMatrix[source][sink] = weight;
-                this.countEdges++;
-            }
-
+    //funcao para retornar os nos de cada linhas
     public int degree (int node){
     int degree = 0;
     for (int j = 0; j < this.adjMatrix[node].length; ++j) {
@@ -54,8 +53,42 @@ public class Graph {
         return degree;
     }
 
-//criar um projeto repoositorio no github
-// postar no moodle com o link
+    //funcao para retornar o maior no grau
+    public int highestDegree(){
+
+        int highI = 0;
+        for(int i = 0; i < adjMatrix.length; ++i ){
+            int deGreeNodeI = this.degree(i);
+            if(highI < deGreeNodeI)
+                highI = deGreeNodeI;
+        }
+    return highI;
+    }
+
+    //funcao para retornar o menos no grau
+    public int lowestDegree(){
+        int LoweJ = 0;
+        for(int i = 0; i > adjMatrix.length; ++i ){
+            int deGreeNodeJ = this.degree(i);
+            if(LoweJ < deGreeNodeJ)
+                LoweJ = deGreeNodeJ;
+        }
+        return LoweJ;
+    }
+
+    //fazer o grafico completar, do grafico existente, ou seja, fazer as ligações que restam do primeiro
+    //obs: Nao mexer na diagonal principal
+    public Graph complement(){
+        Graph graphC = new Graph(countNodes);
+        for(int i = 0; i < this.adjMatrix.length; i++){
+            for( int j = 0; j < this.adjMatrix[i].length; j++){
+                if (i != j && this.adjMatrix[i][j] == 0){
+                    graphC.addEdge(i,j,1);
+                }
+            }
+        }
+        return graphC;
+    }
 
 
 }
--- src/GraphAlgorithms/Main.java
+++ src/GraphAlgorithms/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Graph g1 = new Graph(4);
-
+        System.out.println("******************** GRAFO COMPLETO ********************");
         g1.addEdge(0,1,3);
         g1.addEdge(1,0,3);
         g1.addEdge(0,3,4);
@@ -12,9 +12,20 @@ public class Main {
         g1.addEdge(3,4,2);//aviso
 
         System.out.println(g1);
+        System.out.println("******************** LISTAGENS DE GRAUS ********************");
         System.out.println(g1.degree(0));
         System.out.println(g1.degree(1));
         System.out.println(g1.degree(2));
         System.out.println(g1.degree(3));
+
+        System.out.println("******************** MAIOR GRAU ********************");
+        System.out.println(g1.highestDegree());
+
+        System.out.println("******************** MENOR GRAU ********************");
+        System.out.println(g1.lowestDegree());
+
+        System.out.println("******************** GRAFO COMPLEMENTAR ********************");
+        System.out.println(g1.complement());
+
     }
 }
