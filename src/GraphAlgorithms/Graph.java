96b0c52c5657a7092b79bd655bdc90a4606f68d8
Matheus Silva Corradi <matheus.corradi@aluno.ufop.edu.br>
2022-08-29 23:12:13 -0300

    atv pratica 3

--- src/GraphAlgorithms/Graph.java
+++ src/GraphAlgorithms/Graph.java
@@ -1,8 +1,11 @@
-package GraphAlgorithms;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Graph {
 
-    //contadores
     private int countNodes;
     private int countEdges;
     private int[][] adjMatrix;
@@ -12,15 +15,41 @@ public class Graph {
         this.adjMatrix = new int[countNodes][countNodes];
     }
 
+    public Graph(String fileName) throws IOException {
+        File file = new File(fileName);
+        FileReader reader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
+        // Read header
+        String[] line = bufferedReader.readLine().split(" ");
+        this.countNodes = (Integer.parseInt(line[0]));
+        int fileLines = (Integer.parseInt(line[1]));
+
+        // Create and fill adjMatrix with read edges
+        this.adjMatrix = new int[this.countNodes][this.countNodes];
+        for (int i = 0; i < fileLines; ++i) {
+            String[] edgeInfo = bufferedReader.readLine().split(" ");
+            int source = Integer.parseInt(edgeInfo[0]);
+            int sink = Integer.parseInt(edgeInfo[1]);
+            int weight = Integer.parseInt(edgeInfo[2]);
+            addEdge(source, sink, weight);
+        }
+        bufferedReader.close();
+        reader.close();
+    }
+
     public int getCountNodes() {
         return this.countNodes;
     }
+
     public int getCountEdges() {
         return this.countEdges;
     }
+
     public int[][] getAdjMatrix() {
         return this.adjMatrix;
     }
+
     public String toString() {
         String str = "";
         for (int i = 0; i < this.adjMatrix.length; ++i) {
@@ -32,63 +61,167 @@ public class Graph {
         return str;
     }
 
-    //funcao para adicioanr arestas
     public void addEdge(int source, int sink, int weight) {
-        if (source < 0 || source > this.countNodes - 1 || sink < 0 || sink > this.countNodes - 1 || weight <= 0) {
-            System.err.println("Valor inválido para fonte, coletor ou peso!");
+        if (source < 0 || source > this.countNodes - 1
+                || sink < 0 || sink > this.countNodes - 1 || weight <= 0) {
+            System.err.println("Invalid edge: " + source + sink + weight);
             return;
         }
         this.adjMatrix[source][sink] = weight;
         this.countEdges++;
     }
 
-    //funcao para retornar os nos de cada linhas
-    public int degree (int node){
-    int degree = 0;
-    for (int j = 0; j < this.adjMatrix[node].length; ++j) {
-        if (this.adjMatrix[node][j] !=0)
-            ++degree;
+    public void addEdgeUnoriented(int u, int v, int w) {
+        if (u < 0 || u > this.countNodes - 1
+                || v < 0 || v > this.countNodes - 1 || w <= 0) {
+            System.err.println("Invalid edge: " + u + v + w);
+            return;
         }
+        this.adjMatrix[u][v] = w;
+        this.adjMatrix[v][u] = w;
+        this.countEdges += 2;
+    }
 
+    public int degree(int node) {
+        if (node < 0 || node > this.countNodes - 1)
+            System.err.println("Invalid node: " + node);
+        int degree = 0;
+        for (int j = 0; j < this.adjMatrix[node].length; ++j) {
+            if (this.adjMatrix[node][j] != 0)
+                ++degree;
+        }
         return degree;
     }
 
-    //funcao para retornar o maior no grau
-    public int highestDegree(){
-
-        int highI = 0;
-        for(int i = 0; i < adjMatrix.length; ++i ){
-            int deGreeNodeI = this.degree(i);
-            if(highI < deGreeNodeI)
-                highI = deGreeNodeI;
+    public int highestDegree() {
+        int highest = 0;
+        for (int i = 0; i < this.adjMatrix.length; ++i) {
+            int degreeNodeI = this.degree(i);
+            if (highest < degreeNodeI)
+                highest = degreeNodeI;
         }
-    return highI;
+        return highest;
     }
 
-    //funcao para retornar o menos no grau
-    public int lowestDegree(){
-        int LoweJ = 0;
-        for(int i = 0; i > adjMatrix.length; ++i ){
-            int deGreeNodeJ = this.degree(i);
-            if(LoweJ < deGreeNodeJ)
-                LoweJ = deGreeNodeJ;
+    public int lowestDegree() {
+        int lowest = this.countNodes;
+        for (int i = 0; i < this.adjMatrix.length; ++i) {
+            int degreeNodeI = this.degree(i);
+            if (lowest > degreeNodeI)
+                lowest = degreeNodeI;
         }
-        return LoweJ;
+        return lowest;
     }
 
-    //fazer o grafico completar, do grafico existente, ou seja, fazer as ligações que restam do primeiro
-    //obs: Nao mexer na diagonal principal
-    public Graph complement(){
-        Graph graphC = new Graph(countNodes);
-        for(int i = 0; i < this.adjMatrix.length; i++){
-            for( int j = 0; j < this.adjMatrix[i].length; j++){
-                if (i != j && this.adjMatrix[i][j] == 0){
-                    graphC.addEdge(i,j,1);
+    public Graph complement() {
+        Graph g2 = new Graph(this.countNodes);
+        for (int i = 0; i < this.adjMatrix.length; ++i) {
+            for (int j = 0; j < this.adjMatrix[i].length; ++j) {
+                if (i != j && this.adjMatrix[i][j] == 0) {
+                    g2.addEdge(i, j, 1);
                 }
             }
         }
-        return graphC;
+        return g2;
     }
 
+    public float density() {
+        return (float) this.countEdges / (this.countNodes * (this.countNodes - 1));
+    }
+
+    public boolean subGraph(Graph g2) {
+        if (g2.countNodes > this.countNodes || g2.countEdges > this.countEdges)
+            return false;
+        for (int i = 0; i < g2.adjMatrix.length; ++i) {
+            for (int j = 0; j < g2.adjMatrix[i].length; ++j) {
+                if (g2.adjMatrix[i][j] != 0 && this.adjMatrix[i][j] == 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public ArrayList<Integer> bfs(int s) { // busca em largura
+        // initialization
+        int[] desc = new int[this.countNodes];
+        ArrayList<Integer> Q = new ArrayList<>();
+        Q.add(s);
+        ArrayList<Integer> R = new ArrayList<>();
+        R.add(s);
+        desc[s] = 1;
+        // main loop
+        while (Q.size() > 0) {
+            int u = Q.remove(0);
+            for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+                if (this.adjMatrix[u][v] != 0) { // v é adjacente a u
+                    if (desc[v] == 0) {
+                        Q.add(v);
+                        R.add(v);
+                        desc[v] = 1;
+                    }
+                }
+            }
+        }
+        return R;
+    }
+
+    public ArrayList<Integer> dfs(int s) { // busca em profundidade
+        // initialization
+        int[] desc = new int[this.countNodes];
+        ArrayList<Integer> S = new ArrayList<>();
+        S.add(s);
+        ArrayList<Integer> R = new ArrayList<>();
+        R.add(s);
+        desc[s] = 1;
+        // main loop
+        while (S.size() > 0) {
+            int u = S.get(S.size() - 1);
+            boolean unstack = true; // desempilhar
+            for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+                if (this.adjMatrix[u][v] != 0 && desc[v] == 0) {
+                    S.add(v);
+                    R.add(v);
+                    desc[v] = 1;
+                    unstack = false;
+                    break;
+                }
+            }
+            if (unstack) {
+                S.remove(S.size() - 1);
+            }
+        }
+        return R;
+    }
+
+    public boolean connected() {
+        return this.bfs(0).size() == this.countNodes;
+    }
+
+    public boolean nonOriented() {
+        for (int i = 0; i < this.adjMatrix.length; ++i) {
+            for (int j = i + 1; j < this.adjMatrix.length; ++j) {
+                if (this.adjMatrix[i][j] != this.adjMatrix[j][i])
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public ArrayList<Integer> dfsRec(int s) {
+        int[] desc = new int[this.countNodes];
+        ArrayList<Integer> R = new ArrayList<>();
+        dfsRecAux(s, desc, R);
+        return R;
+    }
+
+    public void dfsRecAux(int u, int[] desc, ArrayList<Integer> R) {
+        desc[u] = 1;
+        R.add(u);
+        for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+            if (this.adjMatrix[u][v] != 0 && desc[v] == 0) {
+                dfsRecAux(v, desc, R);
+            }
+        }
+    }
 
 }
